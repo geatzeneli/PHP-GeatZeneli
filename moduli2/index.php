cd0a2736a31f531ff5fae1bc0f08d4941ca585e9
geatzeneli <geatzeneli03@gmail.com>
2025-11-12 18:50:16 +0100

    moduli2

---
+++ moduli2/index.php
@@ -0,0 +1,21 @@
+<?php
+  
+//   $school="Digital School";
+//   echo $school;
+  
+  // $x=120;
+  // $y=50;
+  // echo $x+$y."<br>";
+  // echo $x-$y."<br>";
+  // echo $x*$y."<br>";
+  // echo $x/$y."<br>";
+
+  $programming="Programming is not cool";
+
+  // echo str_replace("not", "very", $programming)."<br>";
+  // echo str_word_count($programming)."<br>";
+  // echo strlen($programming)."<br>";
+
+  echo strrev($programming);
+
+?>
