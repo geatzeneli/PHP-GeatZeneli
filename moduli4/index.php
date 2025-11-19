0e68bfa460243249b585a9876482b3a9d1e6eced
geatzeneli <geatzeneli03@gmail.com>
2025-11-19 19:04:23 +0100

    moduli4

---
+++ moduli4/index.php
@@ -0,0 +1,55 @@
+<?php 
+//  pfpinfo();
+//  $x=1.5;
+//  echo gettype($x);
+
+// function helloWorld(){
+//     echo "Hello World";
+// }
+
+// helloworld();
+
+// function sum($x=2,$y=4){
+//     echo $x+$y;
+// }
+
+// sum();
+
+// function maximum($x,$y){
+//     if($x>$y){
+//         return $x;
+//     }else{
+//         return $y;
+//     }
+// }
+
+// $a=10;
+// $b=6;
+
+// $test=maximum($a,$b);
+// echo "The max of $a and $b is $test";
+
+// $x=10;
+// function localVariable(){
+//     $y=5;
+// }
+// echo $x;
+
+// $sports=['Futboll','Volleyball','Basketball','Handball'];
+// echo $sports[0]
+
+// for($i=0,i<4;$i++){
+//     echo $sports[$i]."<br>";
+// }
+
+// array_push($sports,'Tenis');
+// array_pop($sports);
+// array_unshift($sports,'Tenis');
+// array_shift($sports);
+
+// $output=array_slice($sports,2);
+// $output=array_slice($sports,2,2);
+// $myvalue=[1,2,3,4,5,6];
+// var_dump(array_sum($myvalue));
+// var_dump($output);
+?>
