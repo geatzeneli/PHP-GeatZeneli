44599c7888333dd6f4a9090e3ec7bc4a0172e1da
geatzeneli <geatzeneli03@gmail.com>
2026-01-19 19:09:55 +0100

    MMS

--- moduliMovieManagement/register.php
+++ moduliMovieManagement/register.php
@@ -1,7 +1,9 @@
 <?php
 
 include_once('config.php');
-if(isset($_POST['sumbit'])){
+
+
+if(isset($_POST['submit'])){
     $emri=$_POST['emri'];
     $username=$_POST['username'];
     $email=$_POST['email'];
@@ -10,4 +12,31 @@ if(isset($_POST['sumbit'])){
     $tempConfirm=$_POST['confirm_password'];
     $confirm_password=password_hash($tempConfirm,PASSWORD_DEFAULT);
     $is_admin=$_POST['is_admin'];
+
+
+    if(empty($emri) || empty($username) || empty($email) || empty($password) || empty($confirm_password) || empty($is_admin)){
+        echo "You have not filled all the fields";
+    }else{
+
+
+        $sql="INSERT INTO users (emri,username,email,password,confirm_password,is_admin) VALUES (:emri,:username,:email,:password,:confirm_password,:is_admin)";
+
+
+        $insertSQL=$conn->prepare($sql);
+
+
+        $insertSQL->bindParam(':emri',$emri);
+        $insertSQL->bindParam(':username',$username);
+        $insertSQL->bindParam(':email',$email);
+        $insertSQL->bindParam(':password',$password);
+        $insertSQL->bindParam(':confirm_password',$confirm_password);
+        $insertSQL->bindParam(':is_admin',$is_admin);
+
+
+        $insertSQL->execute();
+
+
+        header('Location:login.php');
+    
+    }
 }
